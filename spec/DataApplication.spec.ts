73d8ca96382a85bdd7487c21c4800fb83fb9ae9c
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2022-10-08 14:43:09 +0300

    add jest testing environment

--- spec/DataApplication.spec.ts
+++ spec/DataApplication.spec.ts
@@ -26,7 +26,7 @@ describe('DataApplication', () => {
         expect(items).toBeTruthy();
     });
 
-    fit('should seed data', async () => {
+    it('should seed data', async () => {
         const newContext = app.createContext();
         const Users = newContext.model(User);
         await Users.silent().save({
